afdb4858c2fa389d5c57b44c03ddd61cd03de8f1
Ismail Nurudeen <ibrightstar247@gmail.com>
2019-12-14 18:25:55 +0100

    Touched up Settings page

--- app/src/main/java/cc/properton/ui/settings/SettingsFragment.kt
+++ app/src/main/java/cc/properton/ui/settings/SettingsFragment.kt
@@ -1,13 +1,16 @@
 package cc.properton.ui.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import cc.properton.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment() {
 
@@ -26,5 +29,9 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity!!.toolbar_title.text = "Settings"
+        val auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+        settings_phone_num.text = currentUser?.phoneNumber
     }
 }
